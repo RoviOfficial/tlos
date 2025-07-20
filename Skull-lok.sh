4dac974384cb0ee96aa703851e45c525ad4eb756
RoviOfficial <rovisadat@gmail.com>
2025-07-20 12:22:23 +0700

    Create Skull-lok.sh

---
+++ Skull-lok.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pkg install figlet toilet lolcat -y
+
+echo 'clear' > ~/.bashrc
+echo 'figlet "TLOS TENGKORAK" | lolcat' >> ~/.bashrc
+echo 'echo "☠️☠️☠️☠️☠️☠️☠️☠️☠️" | lolcat' >> ~/.bashrc
+echo 'echo "By Rovi Sadat" | lolcat' >> ~/.bashrc
+echo 'echo "☠️ Welcome to the Skull Mode ☠️" | lolcat' >> ~/.bashrc
+echo 'echo "☠️☠️☠️☠️☠️☠️☠️☠️☠️" | lolcat' >> ~/.bashrc
+
+clear
+echo "✅ Tampilan tengkorak berhasil diatur!"
