32dba66979d27847a12c3ad496d0134e0f8d21d6
RoviOfficial <rovisadat@gmail.com>
2025-07-20 12:47:59 +0700

    Update tlos.sh

--- tlos/tlos.sh
+++ tlos/tlos.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
 clear
-echo "============================="
-echo "      🔥 TLOS SYSTEM 🔥      "
-echo "============================="
+echo "==============================="
+echo "🔥 TLOS SYSTEM 🔥"
+echo "==============================="
 echo "1. Ubah Tampilan Termux Keren"
 echo "2. Spam PAIRING WhatsApp"
 echo "3. Ubah Tampilan Tengkorak"
 echo "0. Keluar"
-echo "============================="
+echo "==============================="
 read -p "Pilih menu: " menu
 
 case $menu in
-  1) bash modern-look.sh ;;
-  2) bash pairing-spam.sh ;;
-  3) bash skull-look.sh ;;
-  0) exit ;;
-  *) echo "Pilihan tidak valid!" ;;
+    1)
+        bash keren-look.sh
+        ;;
+    2)
+        bash spam-pairing.sh
+        ;;
+    3)
+        bash skull-look.sh
+        ;;
+    0)
+        exit
+        ;;
+    *)
+        echo "Pilihan tidak valid"
+        ;;
 esac
